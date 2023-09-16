f1f3813adfc3e7f460d638b56ebe72005c7375c3
Ahmed Khaled <ahmed.khaled.fawzy@hotmail.com>
2023-09-17 00:43:10 +0300

    finalizing update

--- resources/views/chrips/edit.blade.php
+++ resources/views/chrips/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-5 lg:p-8">
-        <h2 class="text-4xl">Edit View</h2>
+    <div class="max-w-3xl mx-auto p-4 sm:p-5 lg:p-8">
+        <h2 class="text-3xl mb-5">Edit Chirp</h2>
         <form action="{{ route('chrips.update', $chrip) }}" method="POST">
             @csrf
             @method('patch')
@@ -8,12 +8,13 @@
                 class="block w-full border-gray-600 focus:border-indigo-300 focus:ring focus:ring-indigo-300 focus:ring-opacity-60 rounded shadow-sm "
                 name="message" rows="5">
             {{ old('message', $chrip->message) }}
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
-                <div class="mt-4 space-x-2">
-                    <x-primary-button>{{ __('SAVE') }}</x-primary-button>
-                    <a href="{{route('chrips.index')}}">{{ __('Cancel') }}</a>
-                </div>
+
         </textarea>
+            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            <div class="mt-4 space-x-2">
+                <x-primary-button>{{ __('Update') }}</x-primary-button>
+                <a href="{{ route('chrips.index') }}">{{ __('Cancel') }}</a>
+            </div>
 
         </form>
     </div>
