dceb4f664db4a7f3e56edb16cfe79461b4fea449
Ahmed Khaled <ahmed.khaled.fawzy@hotmail.com>
2023-09-16 23:40:36 +0300

    slight UI changes

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -17,13 +17,13 @@
                             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
                     <div class="flex-1">
-                        <div class="flex justify-start items-center">
+                        <div class="inline-flex justify-start items-center italic leading-tight">
 
-                                <span class="text-gray-700">{{ $chirp->user->name }}</span>
-                                <small class="ml-2 text-sm text-gray-500">{{$chirp->created_at->format('j M Y, g:i a')}}</small>
+                                <span class="text-gray-600">{{ $chirp->user->name }}</span>
+                                <small class="ml-2 text-sm text-gray-400">{{$chirp->created_at->format('j M Y, g:i a')}}</small>
 
                         </div>
-                        <p class="text-2xl mt-1 text-gray-950">{{$chirp->message}}</p>
+                        <p class="text-xl mt-3 text-gray-800">{{$chirp->message}}</p>
                     </div>
                 </div>
             @endforeach
